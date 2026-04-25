bfcd5b0abb26811d5f2f7a48f78952d28071b380
Mertcan <64431136+mertcanyksel@users.noreply.github.com>
2026-04-25 10:36:02 +1000

    Update Book.h
    Added string dateTime

--- Book.h
+++ Book.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <ctime>
 using namespace std;
 
 class Book {
@@ -7,33 +9,24 @@ public:
     string author;
     int ISBN;
     bool availability;
+    string dateTime;
+    
 
-    //    void setBookDetails(string title, string author, int ISBN, bool availability){
-    //        this->title=title;
-    //        this->author=author;
-    //        this->ISBN=ISBN;
-    //        this->availability=availability;
-    //    }
-    //
-
-
-    // Implemented in book.cpp
-    void setBookDetails(string title_b, string author_b, int ISBN_b, bool availability_b);
+    void setBookDetails(string title_b, string author_b, int ISBN_b, bool availability_b, string dateTime);
 
     void displayBookDetails();
-
+    
     void borrowBook();
-
+    
     void returnBook();
-
-
+    
+    
     void new_book();
+    
+    string get_Date();
+    //void insertSort(vector<Book>& library);
 
-    //    void set_BookDetails(string title_b, string author_b, int ISBN_b, bool availability_b) {
-    //        title = title_b;
-    //        author = author_b;
-    //        ISBN= ISBN_b;
-    //        availability = availability_b;
+};
     //    }
 
 
